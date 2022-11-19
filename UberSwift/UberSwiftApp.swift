1ba04cb619a2a8ecc60c2098d828702142304c79
BEK ROZ <bekrozikoff@gmail.com>
2022-11-19 16:38:59 +0500

    Initial Commit

---
+++ UberSwift/UberSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftApp.swift
+//  UberSwift
+//
+//  Created by BekRoz on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

f369de007c8672d116f38f415e5df5a5dca1caef
BEK ROZ <bekrozikoff@gmail.com>
2022-11-19 17:36:03 +0500

    upd: location manager

---
+++ UberSwift/Managers/LocationManager.swift
@@ -0,0 +1,27 @@
+//
+//  LocationManager.swift
+//  UberSwift
+//
+//  Created by BekRoz on 19/11/22.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
+    }
+}
